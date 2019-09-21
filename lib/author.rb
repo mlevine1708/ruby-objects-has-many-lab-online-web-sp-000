339d8e59cb225500a26be61887684701cf26b6c2
Micah Levine <micah.levine@outlook.com>
2019-09-21 21:25:44 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -18,20 +18,18 @@ end
     post.author = self
   end
 
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
+  end
+  
 def posts
   Post.all.select {|post| post.author == self}
 end
 
-def add_post_by_title(title)
-  post = Post.new(title)
-  post.author = self
-end
-
-def title
-
-end
 
 def self.post_count
   Post.all.count
-end
+  end
+  
 end
