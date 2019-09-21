b729aa57a58acfbc62863183a05b7fc915bb6c36
Micah Levine <micah.levine@outlook.com>
2019-09-21 21:27:10 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,16 +1,11 @@
 class Post
 
-attr_accessor :author, :name
+attr_accessor :author, :title
 
 @@all = []
 
 def initialize(name)
-  @author = author
-  @name = name
-  save
-end
-
-def save
+  @title = title
   @@all << self
 end
 
@@ -19,11 +14,7 @@ def self.all
 end
 
 def author_name
-  if author
-    self.author.name
-  else
-    nil
-  end
+  author.name if author
 end
 
 end
