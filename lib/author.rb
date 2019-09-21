cfabc94f56695109fe0b940be4d3f2b7a556487b
Micah Levine <micah.levine@outlook.com>
2019-09-21 21:24:14 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,13 +1,18 @@
 class Author
 
-  attr_accessor :name, :post, :title
+  attr_accessor :name
 
-@@post_count = 3
+
+@@all = []
 
   def initialize(name)
     @name = name
-    @title = title
+    @@all << self
   end
+
+def self.all
+  @@all
+end
 
   def add_post(post)
     post.author = self
@@ -19,18 +24,14 @@ end
 
 def add_post_by_title(title)
   post = Post.new(title)
-  @posts << post
   post.author = self
-  @@post_count += 1
-end
-
-def self.post_count
-  @@post_count
 end
 
 def title
-end
+
 end
 
-
+def self.post_count
+  Post.all.count
+end
 end
